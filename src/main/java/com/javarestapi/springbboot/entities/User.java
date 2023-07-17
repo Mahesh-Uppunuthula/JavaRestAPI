e5f8fe62e9d759aa4c67a1b5d76b831f222331c7
Mahesh-Uppunuthula <uppunuthulamahesh312@gmail.com>
2023-07-18 03:11:26 +0530

    use LOMBOK for USER entity

--- src/main/java/com/javarestapi/springbboot/entities/User.java
+++ src/main/java/com/javarestapi/springbboot/entities/User.java
@@ -1,12 +1,14 @@
 package com.javarestapi.springbboot.entities;
 
-import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 @Entity(name = "USERS")
 public class User {
 	@Id
@@ -14,64 +16,4 @@ public class User {
 	private int Id;
 	private String email;
 	private String password;
-	
-	public User() {}
-
-	public User(int id, String email, String password) {
-		super();
-		Id = id;
-		this.email = email;
-		this.password = password;
-	}
-
-	public int getId() {
-		return Id;
-	}
-
-	public void setId(int id) {
-		Id = id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	
-	
-	@Override
-	public String toString() {
-		return "User [Id=" + Id + ", email=" + email + ", password=" + password + "]";
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(Id, email, password);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Id == other.Id && Objects.equals(email, other.email) && Objects.equals(password, other.password);
-	}
-
-	
-	
 }
