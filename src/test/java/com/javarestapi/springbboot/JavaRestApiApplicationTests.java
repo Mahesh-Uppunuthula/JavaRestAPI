53dd32b3b1805b8295b85b4c12d4240f94f3b75e
Mahesh-Uppunuthula <uppunuthulamahesh312@gmail.com>
2023-07-18 01:51:39 +0530

    inital commit

---
+++ src/test/java/com/javarestapi/springbboot/JavaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarestapi.springbboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
